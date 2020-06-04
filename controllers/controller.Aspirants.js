a070bef9e6c03423be5b615a9b4c488c2331ca8e
Jhonatan Arenas <55113840+cybernuki@users.noreply.github.com>
2020-06-04 15:03:06 -0500

    Create users (#1)
    * implement routes for aspirants and integrate with database
    
    * comment routes responses and requests body
    
    * create new controllers to handle aspirants login and refactor POST /aspirants endpoints to recive email and password and use the controller to execute the login funcionts

---
+++ controllers/controller.Aspirants.js
@@ -0,0 +1,126 @@
+const { Aspirants } = require('../database')
+const axios = require('axios').default
+
+
+// get all aspirant
+const getAll = async () => {
+  const aspirants = await Aspirants.findAll({})
+  return aspirants
+}
+
+// get by id
+const getAspirantById = async (aspirantId) => {
+  const aspirants = await Aspirants.findOne({
+    where: {
+      storeKeeperId: aspirantId
+    }
+  })
+  return aspirants
+}
+
+
+// Register new aspirant
+const registerAspirant = async (data) => {
+  try {
+    const aspirants = await Aspirants.create({
+      storeKeeperId: data.storeKeeperId,
+      email: data.email
+    })
+    return aspirants
+  } catch (error) {
+
+    if (error.errors[0].type == 'unique violation') return null
+    console.error(error);
+  }
+}
+
+/**
+ * Verify if a given email is a courier
+ * 
+ * returns 'none' if it is not a courier, and 'courier' in otherwise.
+ */
+const verifyEmail = async (email) => {
+  try {
+    const response = await axios.get(`http://microservices.dev.rappi.com/api/rt-auth-helper/user/type?email=${email}`);
+    return response.data.user_type;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * getAspirantToken get the aspirant token
+ * @param {*} email 
+ * @param {*} password 
+ * 
+ * return undefine if it was not possible to get the token, otherwise, returns the token.
+ */
+const getAspirantToken = async (email, password) => {
+  const data = {
+    'client_id': '74HzD01JbhZ44iE1kh7Gt6dfNjEKrtWiz0FqTUDQ',
+    'client_secret': 'W8dOKF1mdHaG9wBNyoOCEBgHajO66GEl81lTDu2P',
+    'username': email,
+    'password': password,
+    'scope': 'all'
+  }
+  const options = {
+    'method': 'POST',
+    'headers': {
+      'uuid': '550e8400-e29b-41d4-a716-4466554400001234567',
+      'platform': 2,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const response = await axios.post('http://microservices.dev.rappi.com/api/login/storekeeper', data, options);
+    return response.data.access_token;
+  } catch (error) {
+    //console.log(error);
+  }
+};
+
+
+/**
+ * 
+ * @param {*} token 
+ */
+const getStoreKeeperId = async (token) => {
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  }
+
+  try {
+    const response = await axios.get('http://microservices.dev.rappi.com/api/storekeepers-ms/storekeeper/rappitendero/profile?cache=false', options)
+    return response.data.id;
+  } catch (error) {
+    //console.log(error);
+  }
+}
+
+/**
+ * 
+ * @param {*} email 
+ * @param {*} password 
+ */
+const aspirantLogin = async (data) => {
+  const verify = await verifyEmail(data.email);
+  if (verify === 'none') return { status: 'not_courier' };
+
+  const token = await getAspirantToken(data.email, data.password);
+  if (!token) return { status: 'invalid_credentials' };
+
+  const id = await getStoreKeeperId(token);
+  if (!id) return { status: 'error_getting_id' };
+
+  data.storeKeeperId = id;
+  const aspirant = await registerAspirant(data);
+  if (!aspirant) return { status: 'duplicated' };
+
+  return { status: 'ok' };
+};
+
+module.exports = { getAll, getAspirantById, registerAspirant, registerAspirant, aspirantLogin }
--- routes/aspirants.js
+++ routes/aspirants.js
@@ -1,19 +1,75 @@
 var express = require('express');
 var router = express.Router();
+var { getAll, getAspirantById, registerAspirant, aspirantLogin } = require('../controllers/controller.Aspirants');
 
-/* GET aspirants. */
-router.get('/aspirants', (req, res, next) => {
-  res.json({ message: 'retrive all aspirants' })
+/** GET aspirants. 
+ * This route returns all aspirants saved in the db
+ * response's body:
+ * {
+ *  status: 'ok',
+ *  data: [aspirants]
+ * }
+*/
+router.get('/aspirants', async (req, res, next) => {
+  const aspirants = await getAll()
+  return res.json({
+    status: 'ok',
+    data: aspirants
+  })
 });
 
-/* GET aspirant. */
-router.get('/aspirants/:aspirant_id', (req, res, next) => {
-  res.json({ message: 'retrives an specific aspirant' })
+/** GET aspirant/:aspirant_id 
+ * Execute a search for the given aspirant_id
+ * response's body:
+ * - when user is found:
+ * {
+ *  status: 'ok',
+ *  data: aspirant
+ * }
+ * - when user isn't:
+ * {
+ *  status: 'not_found',
+ *  data: null
+ * }
+*/
+router.get('/aspirants/:aspirant_id', async (req, res, next) => {
+  const aspirant = await getAspirantById(req.params.aspirant_id)
+  const status = (aspirant) ? 'ok' : 'not_found';
+  return res.json({
+    status: status,
+    data: aspirant
+  })
 });
 
-/* POST aspirants. */
-router.post('/aspirants', (req, res, next) => {
-  res.json({ message: 'creates a new aspirant' })
+/** POST aspirants. 
+ * This endpoint login an aspirant to the database.
+ * the response body has the possible status:
+ * 
+ * - When given email is not linked to any rappitendero:
+ * { status: 'not_courier' }
+ * 
+ * - When given email is linked to a rappitendero but password is wrong:
+ * { status: 'invalid_credentials' }
+ * 
+ * - When aspirant is already register in our database:
+ * { status: 'duplicated' }
+ * 
+ * - When everything done correctly:
+ * { status: 'ok' }
+ * 
+ * - When somenthing went wrong trying to get the storekeeper id:
+ * { status: 'error_getting_id'}
+*/
+router.post('/aspirants', async (req, res, next) => {
+  data = req.body;
+  console.log(data);
+
+  if (!data.email || !data.password) return res.status(400).json({ 'message': 'Bad body' });
+
+  let login = await aspirantLogin(data);
+  return res.json({
+    status: login.status,
+  })
 });
 
 module.exports = router;
