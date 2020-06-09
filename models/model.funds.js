607ebfd59dc1dfa501342c9dbc00374a31318a32
Jhonatan <1164@holbertonschool.com>
2020-06-09 16:01:34 -0500

    Add models to support funds entity.
    funds and aspirants has a relation one-to-one
    
    Co-authored-by: Feli <srgatoiscool@gmail.com>

--- models/model.funds.js
+++ models/model.funds.js
@@ -1,20 +1,34 @@
 const FUNDS_STATUTS = ['waiting', 'gathering funds', 'done'];
 
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Funds', {
+  const Funds = sequelize.define('Funds', {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
       primaryKey: true,
-      unique: true,
-      autoIncrement: true
     },
     amount: {
       type: DataTypes.FLOAT,
+      allowNull: false
     },
     status: {
-      type: DataTypes.ENUM(FUNDS_STATUTS)
+      type: DataTypes.ENUM(FUNDS_STATUTS),
+      allowNull: false
     }
   }, {
-    timestamps: false,
+    timestamps: true,
   });
+
+  Funds.associate = (models) => {
+    models.Funds.belongsTo(models.Aspirants, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        name: 'aspirant_id',
+        allowNull: false
+      }
+    });
+  }
+
+  Funds.FUNDS_STATUTS = FUNDS_STATUTS;
+  return Funds;
 };
