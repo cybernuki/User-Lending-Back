e53eba1e7fb87f37908c327894c945704d7f263d
Jhonatan <1164@holbertonschool.com>
2020-06-11 14:04:49 -0500

    Refactor models. A new model is added called piece. We remove the model investor because it is itself contain in pieces model.
    It is possible that the investors endpoint and controller fails.
    
    Co-authored-by: Feli <srgatoiscool@gmail.com>

--- models/model.funds.js
+++ models/model.funds.js
@@ -1,18 +1,20 @@
-const FUNDS_STATUTS = ['waiting', 'gathering funds', 'done'];
+const FUNDS_STATUTS = { waiting: 'waiting', gathering: 'gathering funds', done: 'done' };
 
 module.exports = (sequelize, DataTypes) => {
   const Funds = sequelize.define('Funds', {
     id: {
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV1,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
     amount: {
       type: DataTypes.FLOAT,
+      defaultValue: 0.0,
       allowNull: false
     },
     status: {
-      type: DataTypes.ENUM(FUNDS_STATUTS),
+      type: DataTypes.ENUM(Object.values(FUNDS_STATUTS)),
+      defaultValue: FUNDS_STATUTS['waiting'],
       allowNull: false
     }
   }, {
@@ -24,10 +26,21 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: "CASCADE",
       foreignKey: {
         name: 'aspirant_id',
-        allowNull: false
+        allowNull: false,
+        unique: true
+      }
+    });
+    models.Funds.hasMany(models.Pieces, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        name: 'fund_id',
+        allowNull: false,
+        unique: true,
       }
     });
   }
+
+
 
   Funds.FUNDS_STATUTS = FUNDS_STATUTS;
   return Funds;
