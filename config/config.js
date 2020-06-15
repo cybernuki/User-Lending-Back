036b87f71b9d71b98fee376c94b5702ba4e0d0b2
Jhonatan Arenas <55113840+cybernuki@users.noreply.github.com>
2020-06-15 18:41:44 -0500

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: 'ugveypdjqgy46afa',
-    password: 'DqE8RK3h6HYDRcYpBqzi',
-    database: 'bvoqkfkyktqwe5sxkgpy',
-    host: 'bvoqkfkyktqwe5sxkgpy-mysql.services.clever-cloud.com',
-    dialect: 'mysql',
-  }
-};
