88f56949efd76e315a992d0d2ae5a591fae563c3
Jhonatan Arenas <55113840+cybernuki@users.noreply.github.com>
2020-06-17 19:18:08 -0500

    Investors system (#13)
    * Finish investors' register
    
    Now fund system is complete into the api
    
    * Add investors route

--- controllers/controller.Investors.js
+++ controllers/controller.Investors.js
@@ -1,66 +1,22 @@
-const models = require('../models');
+const { investorsLib } = require('../lib');
+const { putMoney } = investorsLib;
 
 const investorsController = {};
-
-/** - search
- * This function search for a investor by its email
- * @param {*} investorEmail is the investor's email
- * 
- * @returns found investor or null;
- */
-investorsController.search = async (investorEmail) => {
-  const investor = await models.Investors.findOne({
-    where: {
-      email: investorEmail
-    }
-  })
-  return investor;
-}
-
-/** -update
- * This function update the amount of a investor has done.
- * @param {*} email is the email of the investor
- * @param {*} amount is the new amount that will be added
- * 
- * @returns: {result: 'updated'} if it was possible to updated
- * or {result: 'error_to_update'} if it wasn't possible to update
- */
-investorsController.update = async (email, amount) => {
-  try {
-    const result = await models.Investors.increment('amount',
-      {
-        by: amount,
-        where: {
-          email: email
-        }
-      });
-    return { result: 'updated' };
-  } catch (error) {
-    console.log("Error to update investor");
-    return { result: 'error_to_update' };
-  }
-};
 
 /** -create
  * This function creates a new investor into the database
  * @param {*} email is the email of the investor
  * @param {*} amount is the amount that will be added
  * 
- * @returns: {result: 'created', data: investor} if it was possible to create
- * or {result: 'error_to_create'} if it wasn't possible to create
+ * @returns: 
  */
-investorsController.create = async (email, amount) => {
-  try {
-    const investor = await models.Investors.create({
-      email: email,
-      amount: amount
-    });
-    return { result: 'created', data: investor };
-  } catch (error) {
-    console.log(error);
-    return { result: 'error_to_create' };
-  }
-}
+investorsController.registInvestor = async (req, res, next) => {
+  const data = req.body
+  if (!data.email || !data.amount) return res.status(400).json({ 'message': 'Bad body' });
 
+  //registInvestor
+  const result = await putMoney(data.email, data.amount);
+  return res.send(result);
+};
 
 module.exports = investorsController;
--- routes/route.Investors.js
+++ routes/route.Investors.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const investorsUtilities = require('../controllers/controller.Investors'),
-  generalController = require('../controllers');
-
+const { registInvestor } = require('../controllers/controller.Investors');
 /** POST aspirants. 
  * Creates or update an inversor
  * 
 */
-router.post('/investors', async (req, res, next) => {
-  const data = req.body
-  if (!data.email || !data.amount) return res.status(400).json({ 'message': 'Bad body' });
+router.post('/investors', registInvestor);
 
-  //registInvestor
-  const result = await generalController.registInvestor(data);
-  return res.send(result);
-});
-
-router.get('/investors/:user_email', async (req, res, next) => {
-  const investor = await investorsUtilities.search(req.params.user_email);
-  if (!investor) return res.json({ status: 'not_found' });
-  return res.send(investor);
-})
 module.exports = router;
